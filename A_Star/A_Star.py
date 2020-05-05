94f08d3ad9946d2d2ca45429fa430db8786079ce
Giovanni Bernal <gvanni.bernal10@gmail.com>
2020-05-04 19:45:36 -0600

    Python

--- A_Star/A_Star.py
+++ A_Star/A_Star.py
@@ -1,22 +1,38 @@
 from Spot import Spot
 from tkinter import *
+import math
+import time
 
 root = Tk()
-c = Canvas(root, height=400, width=400, bg="white")
+c = Canvas(root, height=600, width=600, bg="white")
+Spot.c = c
 
-rows = 10
-cols = 10
+rows = 50
+cols = 50
 grid = []
+Spot.rows = rows
+Spot.cols = cols
 
 squareHeight = int(c.cget("height")) / rows
 squareWidth = int(c.cget("width")) / cols
+Spot.height = squareHeight
+Spot.width = squareWidth
 
 openSet = []
 closedSet = []
+lowestF = 0
+current = None
+neighbors = None
+tempG = 0
+
+temp = None
+done = False
+
+newPath = False
 
 
 def createCanvas():
-    root.geometry('400x400')
+    root.geometry('600x600')
     c.pack()
 
 
@@ -30,12 +46,35 @@ def createSpotGrid():
 
     for row in range(rows):
         for col in range(cols):
-            print(str(squareHeight) + " " + str(squareWidth))
-            grid[row][col] = Spot(row, col, squareHeight, squareWidth)
+            grid[row][col] = Spot(row, col)
+
+    Spot.grid = grid
 
     for row in range(rows):
         for col in range(cols):
-            grid[row][col].show(c, "white")
+            grid[row][col].addNeighbors()
+
+    for row in range(rows):
+        for col in range(cols):
+            grid[row][col].show()
+
+
+def heuristic(neighbor, end):
+    distance = math.dist([neighbor.y, neighbor.x], [end.y, end.x])
+    # distance = abs(end.x - neighbor.x) + abs(end.y - neighbor.y)
+    return distance
+
+
+def track():
+    path = []
+    temp = current
+    path.append(temp)
+    while temp.parent is not None:
+        path.append(temp.parent)
+        temp = temp.parent
+
+    for spot in path:
+        c.itemconfig(spot.square, fill="blue")
 
 
 createCanvas()
@@ -45,9 +84,66 @@ start = grid[0][0]
 end = grid[rows - 1][cols - 1]
 openSet.append(start)
 
-while len(openSet) > 0:
+while len(openSet) > 0 and not done:
+
+    lowestF = 0
+
+    for index in range(len(openSet)):
+        if openSet[index].f < openSet[lowestF].f:
+            lowestF = index
+
+    current = openSet[lowestF]
+    # current.print(c, "T")
+
+    if current == end:
+        print("Done")
+        done = True
+        break
+
+    openSet.remove(current)
+    closedSet.append(current)
+
+    neighbors = current.neighbors
+
+    for neighbor in neighbors:
+        # neighbor.print(c, "n")
+        if not closedSet.count(neighbor) > 0 and not neighbor.wall:
+            tempG = current.g + 1
+            newPath = False
+
+            if openSet.count(neighbor) > 0:
+                if tempG < neighbor.g:
+                    neighbor.g = tempG
+                    newPath = True
+
+            else:
+                neighbor.g = tempG
+                openSet.append(neighbor)
+                newPath = True
+
+            if newPath:
+                neighbor.h = heuristic(neighbor, end)
+                neighbor.f = neighbor.g + neighbor.h
+                # neighbor.print(str(neighbor.h) + " " + str(neighbor.g) )
+                neighbor.parent = current
+                # time.sleep(0.2)
+
     for spot in openSet:
-        spot.show(c, "green")
+        c.itemconfig(spot.square, fill="green")
+
+    for spot in closedSet:
+        c.itemconfig(spot.square, fill="red")
+
+    track()
 
     root.update_idletasks()
     root.update()
+
+if done:
+    track()
+else:
+    print("No solution")
+    for spot in grid:
+        c.itemconfig(spot.square, fill="red")
+
+mainloop()
--- A_Star/Spot.py
+++ A_Star/Spot.py
@@ -1,24 +1,70 @@
+import random
+
+
 class Spot:
 
-    def __init__(self, row, col, height, width):
+    rows = 0
+    cols = 0
+    height = 0
+    width = 0
+    c = 0
+    grid = 0
+
+
+    def __init__(self, row, col):
         self.x = col
         self.y = row
-        self.height = height
-        self.width = width
         self.g = 0
         self.h = 0
         self.f = 0
         self.square = None
+        self.neighbors = []
+        self.parent = None
+        self.wall = False
+        if random.random() < 0.4 and not (self.x == Spot.cols - 1 and self.y == Spot.rows - 1):
+            self.wall = True
 
         # print(str(self.x) + " " + str(self.y) + "   " + str(self.height) + " " + str(self.width))
 
-    def show(self, c, color):
+    def show(self):
         # print("x: " + str(self.x * self.width) + " to " + str(self.x * self.width + self.width) + "  y: " + str(
         # self.y * self.height) + " to " + str(self.y * self.height + self.height))
 
-        self.square = c.create_rectangle(self.x * self.width, self.y * self.height, self.x * self.width + self.width,
-                                         self.y * self.height + self.height, fill=color)
+        color = "white"
+        if self.wall:
+            color = "black"
+        self.square = Spot.c.create_rectangle(self.x * Spot.width, self.y * Spot.height,
+                                              self.x * Spot.width + Spot.width,
+                                              self.y * Spot.height + Spot.height, fill=color)
 
-    def print(self, c):
-        c.create_text(self.x * self.width + self.width / 2, self.y * self.height + self.height / 2,
-                      text=str(self.x) + ", " + str(self.y))
+    def print(self, text):
+        Spot.c.create_text(self.x * Spot.width + Spot.width / 2, self.y * Spot.height + Spot.height / 2,
+                           text=text)
+
+    def printCoordinates(self):
+        return str(self.y) + " " + str(self.x)
+
+    def addNeighbors(self):
+        col = self.x
+        row = self.y
+
+        if col < Spot.cols - 1:
+            self.neighbors.append(Spot.grid[row][col + 1])
+        if col > 0:
+            self.neighbors.append(Spot.grid[row][col - 1])
+        if row < Spot.rows - 1:
+            self.neighbors.append(Spot.grid[row + 1][col])
+        if row > 0:
+            self.neighbors.append(Spot.grid[row - 1][col])
+
+        if row > 0 and col > 0:
+            self.neighbors.append(Spot.grid[row - 1][col - 1])
+
+        if row > 0 and col < Spot.cols - 1:
+            self.neighbors.append(Spot.grid[row - 1][col + 1])
+
+        if row < Spot.rows - 1 and col < Spot.cols - 1:
+            self.neighbors.append(Spot.grid[row + 1][col + 1])
+
+        if row < Spot.rows - 1 and col > 0:
+            self.neighbors.append(Spot.grid[row + 1][col - 1])
