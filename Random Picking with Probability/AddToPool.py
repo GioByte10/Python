bfeb37d70328450bd8058317c6d83c673a8c690f
Giovanni Bernal <gvanni.bernal10@gmail.com>
2020-10-12 13:04:48 -0600

    Python

---
+++ Random Picking with Probability/AddToPool.py
@@ -0,0 +1,21 @@
+import random
+
+elements = ['A', 'B', 'C']
+probabilities = [0] * 5
+summatory = 0
+
+for i in range(len(elements)):
+    probabilities[i] = int(input("Enter the probability for element " + elements[i] + '\n'))
+    summatory += probabilities[i]
+
+print("There are " + str(summatory) + " items in the pool")
+pool = [0] * summatory
+k = 0
+
+for i in range(len(elements)):
+    for j in range(probabilities[i]):
+        pool[k] = elements[i]
+        k += 1
+
+for i in range(10):
+    print(str(pool[random.randint(0, summatory - 1)]))
--- Random Picking with Probability/venv/Scripts/activate.bat
+++ Random Picking with Probability/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\super\Desktop\Giovanni\Programacion\Lenguajes\Python\Random Picking with Probability\venv
+set VIRTUAL_ENV=C:\Users\super\Desktop\Giovanni\Programacion\Lenguajes\Python\Random Picking With Probability\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
