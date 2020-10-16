e405fb218b07b89044d5e4c2086ffaaaa7d2cd41
Giovanni Bernal <gvanni.bernal10@gmail.com>
2020-10-16 00:45:24 -0600

    Python

--- Random Picking with Probability/Test.py
+++ Random Picking with Probability/Test.py
@@ -1,21 +1,35 @@
 import random
 
-probabilities = [0] * 100
-maxValue = 100
+probabilities = [0] * 3
+probabilities[0] = 1
+probabilities[1] = 3
+probabilities[2] = 6
+maxValue = 6
 
-for num in range(100):
-    probabilities[num] = num + 1
+i = 0
+j = 0
+k = 0
 
 
 def acceptReject():
     while True:
         selectedNum = random.randint(0, len(probabilities) - 1)
         selected = probabilities[selectedNum]
-        rand = random.randint(0, maxValue)
+        rand = random.random() * maxValue
 
         if rand < selected:
             return selected
 
 
-for num in range(100):
-    print(acceptReject())
+for num in range(10000):
+    n = acceptReject()
+    if n == 1:
+        i += 1
+
+    elif n == 3:
+        j += 1
+
+    elif n == 6:
+        k += 1
+
+print("A: " + str(i) + " B: " + str(j) + " C: " + str(k))
