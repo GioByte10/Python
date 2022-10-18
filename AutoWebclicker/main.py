174be3c9991c83aac72fa27ace298b8d4c0a447f
Giovanni Bernal <gvanni.bernal10@gmail.com>
2022-10-18 14:30:25 -0700

    Python

--- AutoWebclicker/main.py
+++ AutoWebclicker/main.py
@@ -1,29 +1,16 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
-
-import time
-from winotify import Notification, audio
 
 import json
 import time
 from random import randint
 
 
-def notify():
-    toast = Notification(app_id="ECE 15",
-                         title="Vote",
-                         msg="At least 60 students have voted",
-                         duration="long",
-                         icon=r"C:\Users\super\Desktop\Giovanni\Programacion\Resources\CheckmarkIcon.webp")
-
-    toast.set_audio(audio.Mail, loop=False)
-    toast.show()
-
-
 def getVoteCount():
-    vote_element = driver.find_element(By.XPATH, "//li[@class='MuiListItem-root MuiListItem-gutters']/div[@class='MuiListItemText-root MuiListItemText-multiline']/p[@class='MuiTypography-root MuiListItemText-secondary MuiTypography-body2 MuiTypography-colorTextSecondary MuiTypography-displayBlock']")
+    vote_element = driver.find_element(By.XPATH,
+                                       "//li[@class='MuiListItem-root MuiListItem-gutters']/div[@class='MuiListItemText-root MuiListItemText-multiline']/p[@class='MuiTypography-root MuiListItemText-secondary MuiTypography-body2 MuiTypography-colorTextSecondary MuiTypography-displayBlock']")
     vote_string = vote_element.text
     print(vote_string)
 
@@ -35,35 +22,36 @@ def getVoteCount():
         return -1
 
 
-def vote(n):
+def vote():
+
+    n = randint(1, 5)
 
     if n == 1:
-        driver.find_element(By.XPATH, "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[@class='MuiButton-label']").click()
+        driver.find_element(By.XPATH,
+                            "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'A')]").click()
         time.sleep(2)
     elif n == 2:
-        driver.find_element(By.XPATH, "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'B')]").send_keys(chr(10))
+        driver.find_element(By.XPATH,
+                            "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'B')]").click()
         time.sleep(2)
     elif n == 3:
-        driver.find_element(By.XPATH, "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'C')]").click()
+        driver.find_element(By.XPATH,
+                            "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'C')]").click()
         time.sleep(2)
     elif n == 4:
-        driver.find_element(By.XPATH, "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'C')]").send_keys('\n')
+        driver.find_element(By.XPATH,
+                            "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'D')]").click()
         time.sleep(2)
     elif n == 5:
-        driver.find_element(By.XPATH, "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'E')]").click()
+        driver.find_element(By.XPATH,
+                            "//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButtonGroup-grouped MuiButtonGroup-groupedHorizontal MuiButtonGroup-groupedOutlined MuiButtonGroup-groupedOutlinedHorizontal MuiButtonGroup-groupedOutlined']/span[contains(text(), 'E')]").click()
 
 
 with open('info.json') as json_file:
     data = json.load(json_file)
 
-'''
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-'''
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.maximize_window()
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
 link = "https://webclicker.web.app/login/"
 driver.get(link)
@@ -81,12 +69,13 @@ time.sleep(5)
 
 driver.find_element(By.XPATH, "//div[@class='MuiButtonBase-root MuiListItem-root MuiListItem-gutters MuiListItem-button']").click()
 
+
 while True:
     votes = getVoteCount()
     print(votes)
 
     if votes > 60:
-        notify()
+        vote()
         temp = votes
 
         while True:
